daa72e3ec4f0d82299cb0896a30f398f9eb16eef
Sudhanshu Baluja <sbaluja1026@gmail.com>
2026-01-06 23:55:49 +0000

    Hide refresh button when monitoring is disabled

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -197,7 +197,7 @@ export default async function DashboardPage() {
                 </p>
               </div>
             </div>
-            {user.googleAccessToken && <SyncButton />}
+            {user.googleAccessToken && isMonitoringEnabled && <SyncButton />}
           </div>
         </div>
 
